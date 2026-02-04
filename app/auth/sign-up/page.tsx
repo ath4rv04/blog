ac285574c4646677f25f738427a9f954775d133c
atharv <atharv.shawrikar.7@gmail.com>
2026-02-05 01:10:35 +0530

    UI for sign up page is done along with some validation

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import { signUpSchema } from "@/app/schemas/auth";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Field, FieldError, FieldLabel } from "@/components/ui/field";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
@@ -17,6 +18,10 @@ export default function signUpPage() {
         },
     });
 
+    function onSubmit() {
+        console.log("sucess");
+    }
+
     return (
         <Card>
             <CardHeader>
@@ -24,18 +29,38 @@ export default function signUpPage() {
                 <CardDescription>Create an account to get started</CardDescription>
             </CardHeader>
             <CardContent>
-                <form>
-                    <Field>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <FieldGroup className="gap-y-4">
                         <Controller name ="name" control={form.control} render = {({field, fieldState}) => (
                             <Field>
                                 <FieldLabel>Full Name</FieldLabel>
-                                <Input placeholder="John Doe" {...field} />
+                                <Input aria-invalid = {fieldState.invalid} placeholder="John Doe" {...field} />
                                 {fieldState.invalid && (
                                     <FieldError errors={[fieldState.error]} />
                                 )}
                             </Field>
                         )} />
-                    </Field>
+                        <Controller name ="email" control={form.control} render = {({field, fieldState}) => (
+                            <Field>
+                                <FieldLabel>Email</FieldLabel>
+                                <Input aria-invalid = {fieldState.invalid} placeholder="john@doe.com" type="email" {...field} />
+                                {fieldState.invalid && (
+                                    <FieldError errors={[fieldState.error]} />
+                                )}
+                            </Field>
+                        )} />
+                        <Controller name ="password" control={form.control} render = {({field, fieldState}) => (
+                            <Field>
+                                <FieldLabel>Password</FieldLabel>
+                                <Input aria-invalid = {fieldState.invalid} placeholder="*********" type="password" {...field} />
+                                {fieldState.invalid && (
+                                    <FieldError errors={[fieldState.error]} />
+                                )}
+                            </Field>
+                        )} />
+
+                        <Button>Sign Up</Button>
+                    </FieldGroup>
                 </form>
             </CardContent>
         </Card>
